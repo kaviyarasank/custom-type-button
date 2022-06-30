464b21e4d8fd117f950f91ef0c87839e997bfb3c
kaviyarasan <kaviyarasan@dorustree.in>
2022-06-30 18:38:49 +0530

    remoce css

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./Button.css";
 
 export interface buttonprops{
 name:string,
@@ -7,7 +6,15 @@ onClick:any,
 height:string,
 width:string,
 borderRadius:string,
-testid:string
+testid:string,
+textAlign:any,
+color:string,
+background:string,
+transition:string,
+boxShadow:string,
+border:string,
+backgroundSize:string,
+
 }
 export function Button(props:buttonprops){
     return(
@@ -15,9 +22,15 @@ export function Button(props:buttonprops){
 <button 
 onClick={props.onClick} 
 style={{height:`${props.height}`,
+textAlign:props.textAlign,
+border:`${props.border}`,
+transition:`${props.transition}`,
+boxShadow:`${props.boxShadow}`,
+color:`${props.color}`,
+background:`${props.background}`,
 width:`${props.width}`,
-borderRadius:`${props.borderRadius}`}} 
-className="choicesbutton" 
+backgroundSize:`${props.backgroundSize}`,
+borderRadius:`${props.borderRadius}`}}  
 data-testid={props.testid} 
 >{props.name}</button>
         </div>
